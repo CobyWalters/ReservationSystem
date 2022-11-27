d375dbe6a37e2f613a41005618f9768920f567b2
Pat Silva <pat-silva-jr@outlook.com>
2022-11-27 01:54:11 -0600

    added labels for inputs

--- src/components/InputSection.js
+++ src/components/InputSection.js
@@ -7,14 +7,22 @@ const InputSection = () => {
     const [value, onChange] = useState(new Date())
     return(
         <div className="inputSectionContainer">
-            <select className="partySelector">
-                <option selected value="1">1 Person</option>
-                <option value="2">2 People</option>
-                <option value="3">3 People</option>
-                <option value="4">4 People</option>
-                <option value="5">5 People</option>
-            </select>
-            <DatePicker onChange={onChange} value={value}className="dateSelector"/>
+            <div className="partyWrapper">
+                <label className="partyLabel">Party Size</label>
+                <select className="partySelector">
+                    <option selected value="1">1 Person</option>
+                    <option value="2">2 People</option>
+                    <option value="3">3 People</option>
+                    <option value="4">4 People</option>
+                    <option value="5">5 People</option>
+                    <option value="6">6 People</option>
+                </select>
+            </div>
+            <div className="dateWrapper">
+                <label className="dateLabel">Date</label>
+                <DatePicker onChange={onChange} value={value}className="dateSelector"/>
+            </div>
+            
         </div>
     )
 }
