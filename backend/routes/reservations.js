661abbe0e0ea833afcf68be1035851f37e0776ac
CobyWalters <cobywalters@protonmail.com>
2022-11-29 10:35:22 -0600

    cleanup

--- backend/routes/reservations.js
+++ backend/routes/reservations.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 let Reservation = require('../models/reservation.model');
 let User = require('../models/user.model');
+let Table = require('../models/table.model');
 const mongoose = require('mongoose');
 
 router.route('/').get(async (req, res) => {
@@ -24,14 +25,13 @@ router.route('/add').post(async (req, res) => {
     const partyFirstName = req.body.firstName;
     const partyLastName = req.body.lastName;
     const phoneNumber = req.body.phoneNumber;
-
     // Add reservation to set of reservations
     try {
 
         //findTables(date, timeSlot);
         const tables = [
             {
-                "tableNumber": 1,
+                "tableNumber": 5,
                 "tableSize": 4
             },
             {
@@ -40,7 +40,7 @@ router.route('/add').post(async (req, res) => {
             }
         ];
 
-        const newReservation = new Reservation({tables, partySize, date, time, username, partyFirstName, partyLastName, phoneNumber});
+        const newReservation = new Reservation({tables, partySize, date, time, partyFirstName, partyLastName, phoneNumber});
         await newReservation.save()
         //if (username != null) {
             //const user = await User.findOne({username: username});
@@ -73,7 +73,7 @@ router.route('/delete').post(async (req, res) => {
 router.route('/getOpenSlots').get(async (req, res) => {
     // Returns availabilities of a certain day's timeslots
     // TO DO: Everything, this is just a mock implementation
-    res.json({
+    /*res.json({
         timeSlots: [
             {
                 time: "1:00 PM",
@@ -157,7 +157,82 @@ router.route('/getOpenSlots').get(async (req, res) => {
             }
         ],
         holdFee: true
-    });
-})
+    });*/
+    
+    const partySize = req.body.partySize;
+    const date = new Date(req.body.date);
+    
+    timeSlots = [
+        "1:00 PM", "1:30 PM", "2:00 PM", "2:30 PM",
+        "3:00 PM", "3:30 PM", "4:00 PM", "4:30 PM",
+        "5:00 PM", "5:30 PM", "6:00 PM", "6:30 PM",
+        "7:00 PM", "7:30 PM", "8:00 PM", "8:30 PM",
+        "9:00 PM", "9:30 PM", "10:00 PM", "10:30 PM"
+    ]
+
+    //timeSlots.forEach(function(timeSlot) {
+        //console.log(timeSlot);
+    //});
+
+    try {
+
+        const reservedTables = await Reservation.aggregate([
+            {$match: {date: date, time: "1:00 PM"}},
+            {$unwind: "$tables"},
+            {$group: {_id: null, tables: {"$addToSet": "$tables"}}},
+            {$project: {_id: 0, tables: {_id: 0}}},
+        ]).then(response => response[0]["tables"]);
+        
+        const allTables = await Table.find().select({_id: 0, tableNumber: 1, tableSize: 1}).sort({tableSize: "ascending"});
+
+        // availableTables is the difference of the set allTables and the set reservedTables
+        const availableTables = allTables.filter(function(a) {
+            return !reservedTables.some(function(b) {
+                return a.tableNumber == b.tableNumber;
+            });
+        });
+        
+        // Search for an available single table
+        for (let i = 0; i < availableTables.length; ++i) {
+            if (availableTables[i].tableSize > partySize * 2) {
+                break;
+            } else if (availableTables[i].tableSize >= partySize) {
+                console.log(availableTables[i]);
+                break;
+            }
+        }
+
+        // Search for an available double table
+        let bestTable1, bestTable2;
+        let bestSize = 1000; //arbitrary max value
+        for (let i = 0; i < availableTables.length; ++i) {
+            for (let j = i + 1; j < availableTables.length; ++j) {
+                const combinedSize = availableTables[i].tableSize + availableTables[j].tableSize;
+                if (combinedSize > partySize * 2)
+                    break;
+                if (combinedSize < partySize)
+                    continue;
+                if (combinedSize >= bestSize)
+                    break; 
+                bestTable1 = availableTables[i];
+                bestTable2 = availableTables[j];
+                bestSize = combinedSize;
+            }
+        }
+
+        console.log(bestTable1, bestTable2);
+
+       
+        //if (reservation == null)
+            //throw 'Reservation does not exist.';
+
+        //await Reservation.deleteOne({'_id': req.body.id})
+        //res.json('Reservation removed!');
+        res.json('boop');
+    } catch (error) {
+        res.status(400).json('Error: ' + error);
+    }
+
+});
 
 module.exports = router;
